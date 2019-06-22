d5438604c31da9fb9e50c22eaceec0a6d5aa57b2
FruitSam <FruitSam@users.noreply.github.com>
2019-06-22 20:58:46 +0300

    Downloading pic from url given

--- tgBot/tgBot.py
+++ tgBot/tgBot.py
@@ -1,28 +1,37 @@
 import telebot
 import time
+import urllib.request
+from PIL import Image
+import requests
+from io import BytesIO
+
 
 bot_token = '835075644:AAERQCEPXSjpc-Z9SvZFPFcbPXNfiLUS3QI'
 bot = telebot.TeleBot(token=bot_token)
 
-def find_at(msg):
-    for text in msg:
-        if '@' in text:
-            return text
+
+def dl(url):
+    f = open('pic.jpg', 'wb')
+    f.write(urllib.request.urlopen(url).read())
+    f.close
+
+
 
 @bot.message_handler(commands = ['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Welcome')
+    bot.reply_to(message, 'Welcome, press /help for more information')
 
 @bot.message_handler(commands = ['help'])
 def send_welcome(message):
-    bot.reply_to(message, 'Send username')
+    bot.reply_to(message, 'Send a url')
 
-@bot.message_handler(func=lambda msg: msg.text is not None and "@" in msg.text)
-def at_answer(message):
-    texts = message.text.split()
-    at_text = find_at(texts)
-
-    bot.reply_to(message, 'https://instagram.com/{}'.format(at_text[1:]))
+@bot.message_handler(func=lambda m: True)
+def send_photo(message): 
+    dl(message.text)
+    bot.send_chat_action(message.chat.id, 'upload photo')
+    img = open('pic.jpg','rb')
+    bot.sendphoto(message.chat.id,img, reply_to_message_id=message.message_id)
+    img.close
 
 while True:
     try:
