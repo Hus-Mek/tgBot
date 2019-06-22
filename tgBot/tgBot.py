4238f558178f05c31bf2e574b53795d9a6c085f2
FruitSam <FruitSam@users.noreply.github.com>
2019-06-23 00:22:47 +0300

    Screw heroku

--- tgBot/tgBot.py
+++ tgBot/tgBot.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 bot_token = '835075644:AAERQCEPXSjpc-Z9SvZFPFcbPXNfiLUS3QI'
 bot = telebot.TeleBot(token=bot_token)
-server = Flask(_name_)
+
 
 def dl(url):
     f = open('pic.jpg', 'wb')
@@ -29,19 +29,10 @@ def send_photo(message):
     img = open('pic.jpg','rb')
     bot.send_photo(message.chat.id,img, reply_to_message_id=message.message_id)
     img.close
-
-@server.route('/' + bot_token, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+def listener(messages):
+    for m in messages:
+        print(str(m))
 
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=' https://botttg.herokuapp.com/' + bot_token)
-    return "!", 200
-
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+bot.set_update_listener(listener)
+bot.polling()
