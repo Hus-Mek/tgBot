ea5fcb56bc6d6a4b04b423a094594b2909863ee4
FruitSam <FruitSam@users.noreply.github.com>
2019-06-22 12:37:21 +0300

    Bot made

--- tgBot/tgBot.py
+++ tgBot/tgBot.py
@@ -1,1 +1,31 @@
+import telebot
+import time
 
+bot_token = '835075644:AAERQCEPXSjpc-Z9SvZFPFcbPXNfiLUS3QI'
+bot = telebot.TeleBot(token=bot_token)
+
+def find_at(msg):
+    for text in msg:
+        if '@' in text:
+            return text
+
+@bot.message_handler(commands = ['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Welcome')
+
+@bot.message_handler(commands = ['help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Send username')
+
+@bot.message_handler(func=lambda msg: msg.text is not None and "@" in msg.text)
+def at_answer(message):
+    texts = message.text.split()
+    at_text = find_at(texts)
+
+    bot.reply_to(message, 'https://instagram.com/{}'.format(at_text[1:]))
+
+while True:
+    try:
+        bot.polling()
+    except Exception:
+        time.sleep(15)
